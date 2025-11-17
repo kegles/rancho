05e65647d6412580b47068bcca5c3a45b97c4ece
Nataniel Kegles <nataniel@kegles.com.br>
2025-11-17 13:20:26 -0300

    - Fix message

--- resources/views/registration/closed.blade.php
+++ resources/views/registration/closed.blade.php
@@ -6,7 +6,7 @@
   <div class="row">
     <div class="col-lg-10 col-xl-8">
       <h1 class="h3 mb-3">Inscrições encerradas</h1>
-        <div class="alert alert-danger">{{ session('error') }}</div>
+        <div class="alert alert-danger">As Inscrições para o evento estão encerradas. Em caso de dúvida, entre em contato com a organização do evento.</div>
     </div>
   </div>
 @endsection
